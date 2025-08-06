720575b2a3cf675286e04fd3dcfa90891fa00b70
Kevin Lin <2004klin@gmail.com>
2025-08-06 14:48:59 -0400

    feat: Add hello

---
+++ README.md
@@ -0,0 +1,20 @@
+# discord bot
+
+```py
+@app_commands.guilds(discord.Object(id=GUILD_ID)) # Place your guild ID here
+```
+Use this for development to instantly sync commands.
+
+## Download steps
+- Clone/Download the repository
+  - `git clone $SSH/HTTPLINK`
+
+## How to set up
+
+- ask for .env file
+
+### Docker
+
+```
+docker-compose up -d --build
+```
---
+++ cogs/fun.py
@@ -0,0 +1,90 @@
+import random
+
+import aiohttp
+import discord
+from discord.ext import commands
+from discord.ext.commands import Context
+from discord import app_commands
+
+class Choice(discord.ui.View):
+    def __init__(self) -> None:
+        super().__init__()
+        self.value = None
+
+    @discord.ui.button(label="Heads", style=discord.ButtonStyle.blurple)
+    async def confirm(
+        self, interaction: discord.Interaction, button: discord.ui.Button
+    ) -> None:
+        self.value = "heads"
+        self.stop()
+
+    @discord.ui.button(label="Tails", style=discord.ButtonStyle.blurple)
+    async def cancel(
+        self, interaction: discord.Interaction, button: discord.ui.Button
+    ) -> None:
+        self.value = "tails"
+        self.stop()
+
+class Fun(commands.Cog, name="fun"):
+    def __init__(self, bot) -> None:
+        self.bot = bot
+
+    @commands.hybrid_command(name="hello", description="Hello")
+    @app_commands.guilds(discord.Object(id=244842516631781386))
+    async def hello(self, context: Context) -> None:
+        embed = discord.Embed(description="Hello", color=0xBEBEFE)
+        await context.send(embed=embed)
+
+    @commands.hybrid_command(name="randomfact", description="Get a random fact.")
+    @app_commands.guilds(discord.Object(id=244842516631781386))
+    async def randomfact(self, context: Context) -> None:
+        """
+        Get a random fact.
+
+        :param context: The hybrid command context.
+        """
+        # This will prevent your bot from stopping everything when doing a web request - see: https://discordpy.readthedocs.io/en/stable/faq.html#how-do-i-make-a-web-request
+        async with aiohttp.ClientSession() as session:
+            async with session.get(
+                "https://uselessfacts.jsph.pl/random.json?language=en"
+            ) as request:
+                if request.status == 200:
+                    data = await request.json()
+                    embed = discord.Embed(description=data["text"], color=0xD75BF4)
+                else:
+                    embed = discord.Embed(
+                        title="Error!",
+                        description="There is something wrong with the API, please try again later",
+                        color=0xE02B2B,
+                    )
+                await context.send(embed=embed)
+
+    @commands.hybrid_command(
+        name="coinflip", description="Make a coin flip, but give your bet before."
+    )
+    @app_commands.guilds(discord.Object(id=244842516631781386))
+    async def coinflip(self, context: Context) -> None:
+        """
+        Make a coin flip, but give your bet before.
+
+        :param context: The hybrid command context.
+        """
+        buttons = Choice()
+        embed = discord.Embed(description="What is your bet?", color=0xBEBEFE)
+        message = await context.send(embed=embed, view=buttons)
+        await buttons.wait()  # We wait for the user to click a button.
+        result = random.choice(["heads", "tails"])
+        if buttons.value == result:
+            embed = discord.Embed(
+                description=f"Correct! You guessed `{buttons.value}` and I flipped the coin to `{result}`.",
+                color=0xBEBEFE,
+            )
+        else:
+            embed = discord.Embed(
+                description=f"Woops! You guessed `{buttons.value}` and I flipped the coin to `{result}`, better luck next time!",
+                color=0xE02B2B,
+            )
+        await message.edit(embed=embed, view=None, content=None)
+
+async def setup(bot) -> None:
+    await bot.add_cog(Fun(bot))
